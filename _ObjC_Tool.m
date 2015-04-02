39a9f05370c8a2cd6b01006de2b99285bf9a948e
Alex Gray <alex@mrgray.com>
2015-04-02 02:50:33 -0400

    one musc't trash their plists

--- _ObjC_Tool.m
+++ _ObjC_Tool.m
@@ -1,151 +1,164 @@
-@import AppKit;
 
-id _system(id cmd) { 
-   return !cmd ? nil : ({ NSPipe* pipe; NSTask * task;
-  [task = NSTask.new setValuesForKeysWithDictionary: 
-    @{ @"launchPath" : @"/bin/zsh",
-        @"arguments" : @[@"-c", cmd],
-   @"standardOutput" : pipe = NSPipe.pipe}]; [task launch];
-  [NSString.alloc initWithData:
-     pipe.fileHandleForReading.readDataToEndOfFile
-                      encoding:NSUTF8StringEncoding]; });
-}
-
+#define      args NSProcessInfo.processInfo.arguments
 #define XcodePath _system(@"printf \"${$(xcode-select -p)%.app/*}.app\"")
+#define      M(X) NSMutable##X
+#define LOC(S,LOC) [S rangeOfString:LOC].location
+#define $(...) [NSString stringWithFormat:__VA_ARGS__]
+#define $APPEND(X,...) [X appendFormat:__VA_ARGS__]
+#define CR(MSTR) [MSTR appendString:@"\n"]
 
-@implementation  NSMutableString (Plist2Header)
+#import "RegExCategories.h"
 
-- (void) if:(NSString*)key do:(void(^)())stuff {
+id _uSort(id); id _sTask(id); static M(Dictionary)* primitives, *pointers, *parenthized, *qualifiers;
 
-  NSUInteger platform = [key rangeOfString: @"_"].location; // look for underscore.
-  NSString *exclusive = platform != NSNotFound ? [key substringFromIndex:platform + 1] : nil;
+@implementation NSString  (Subscript) - objectAtIndexedSubscript:(NSInteger)i { return i < 0 ? [self substringFromIndex:ABS(i)] : [self substringToIndex:i]; } @end
 
-  exclusive ? [self appendFormat:@"\n#if %@\n", exclusive]       : [self appendString:@"\n"];   stuff();
-  exclusive ? [self appendFormat:@"\n#endif // %@\n", exclusive] : [self appendString:@"\n"];
+@implementation M(String) (Plist2Header)
+
+- (void) _if:key do:(void (^)())stuff {
+
+    NSString* exclusive = LOC(key,@"_") == NSNotFound ? nil : key[ - (LOC(key,@"_") + 1) ];
+
+    exclusive ? $APPEND(self, @"\n#if %@ // $@\n\n", exclusive) : CR(self); stuff();
+    exclusive ? $APPEND(self, @"\n#endif // %@\n",   exclusive) : CR(self);
 }
 
-- (void) addDefine:(NSString*)def {  // defines are split at first "space".
+- (void) _addDefine:dstr {
 
-  NSUInteger space = [def rangeOfString:@" "].location;
+    NSUInteger space = LOC(dstr,@" "); // defines are split at first "space".
 
-  [self appendFormat:@"#define %20s %@\n", [[def   substringToIndex:space] UTF8String],
-                                           [def substringFromIndex:space]];
+    $APPEND(self,@"#define %30s %@\n", [dstr[space] UTF8String], dstr[-space]);
 }
 
-- (void) appendMap:(NSDictionary*)defs {
+- (void) _appendMap:defs { M(String)* methArgs, *dereferenceable;
 
-  NSMutableString *methArgs, * dereferenceable;
-
-    methArgs =  @"\n/// Good to make \"shortcuts\" for ALL `_ObjC` types as \"method arguments\"\n"
-       "/// with a leading Underscore to use ase parenthesis-free method parameterts!\n".mutableCopy;
+    methArgs = @"\n/// Good to make \"shortcuts\" for ALL `_ObjC` types as \"method arguments\"\n"
+                "/// with a leading Underscore to use ase parenthesis-free method parameterts!\n".mutableCopy;
 
     dereferenceable = @"/// For ObjC classes, let's define a preprocessor Macro to call the direct Classes, without the _.\n".mutableCopy;
 
-    [defs enumerateKeysAndObjectsUsingBlock:^(id mapping, id map, BOOL *stop) {
+    [defs enumerateKeysAndObjectsUsingBlock:^(id mapping, id map, BOOL* stop) {
 
-      [self if:mapping do:^{
+      [self _if:mapping do:^{
 
-        [self appendFormat:@"#pragma mark - %@ \n\n", mapping];
+        $APPEND(self,@"#pragma mark - %@ \n\n", mapping);
 
         for (NSString * def in map) {
 
-        NSRange range = [def rangeOfString:@" " options:NSBackwardsSearch];
-        NSString *newType = [def substringFromIndex:range.location+1];
+          NSRange     range = [def rangeOfString:@" " options:NSBackwardsSearch];
+          NSString *newType = def[ - (range.location + 1) ],
+                 * standard = def[    range.location      ];
+          BOOL    isPointer = LOC(standard,@"*") != NSNotFound;
 
-           NSString * standard = [def substringToIndex:range.location];
+          (isPointer ? (pointers   = pointers   ?: @{}.mutableCopy) :
+                       (primitives = primitives ?: @{}.mutableCopy))
 
-          [self appendFormat:@"_Type %30s  %@ _\n", standard.UTF8String, newType];
+          [isPointer ? standard[standard.length -2] : standard] = newType;
 
-          if ([@[@"CA", @"NS", @"AV"] containsObject:[standard substringToIndex:2]]) {
+          $APPEND(self,@"_Type %30s  %@ _\n", standard.UTF8String, newType);
+
+          if ([@[@"CA", @"NS", @"AV"] containsObject:[standard substringToIndex:2]] && isPointer) { // dereferenceable
 
             NSString *prefix = [newType substringToIndex:1];
-            [dereferenceable appendFormat:@"\n#define %30s%@  %@", [[newType substringFromIndex:1] UTF8String],
-                  [prefix isEqualToString:@"_"] ? @"" : [prefix uppercaseString],
-                                              [standard substringToIndex:standard.length -2]];
-//            break;
+
+            $APPEND(dereferenceable, @"\n#define %30s%@  %@", [newType[-1] UTF8String],
+
+            [prefix isEqualToString:@"_"] ? @"" : prefix.uppercaseString, standard[standard.length -2]);
           }
 
-          [methArgs appendFormat:@"\n#define %15s_  (%@)", [newType UTF8String], newType];
+          $APPEND(methArgs,@"\n#define %15s_  (%@)", newType.UTF8String, newType);
         }
+      }];
+
     }];
 
-  }];
-
-  [self appendFormat:@"%@\n\n%@", methArgs, dereferenceable];
-}
-
-+ plist2Header:(NSString*)p { NSMutableString *header; return header = [self stringWithFormat:@"\n"
-
-    "/*! @note This is an AUTOMATICALLY generated file!\n"
-    "    Built on %@ from %@ */\n", [NSDateFormatter localizedStringFromDate:NSDate.date
-                                                                     dateStyle:NSDateFormatterMediumStyle
-                                                                    timeStyle:NSDateFormatterMediumStyle], p], ({
-
-  for (id entry in [NSArray arrayWithContentsOfFile:p]) {
-
-    [entry isKindOfClass: NSArray.class] ? [header appendString:[entry componentsJoinedByString:@"\n"] :
-    [entry isKindOfClass:NSString.class] ? [header appendFormat:@"\n%@",entry] :
-
-    [entry enumerateKeysAndObjectsUsingBlock:^(NSString* description, id decodables, BOOL *s) {
-
-        [header appendFormat:@"\n// %@\n\n", description];
-        [
-
-        [kind hasPrefix:@"define"] ?
-
-        [header if:kind do:^{ for (id def in definitions) [header addDefine:def]; }] :
-
-        [kind isEqualToString:@"map"]    ? [header appendMap:definitions] : nil;
-    }];
-  };    }), header;
+    $APPEND(self,@"%@\n\n%@", _uSort(methArgs),_uSort(dereferenceable));
 }
 
 @end
+
+#pragma mark - UTILS
+
+id _hMake(id path) {
+
+  NSCAssert([NSFileManager.defaultManager fileExistsAtPath:path isDirectory:NULL], @"need plist");
+
+  M(String) *head = [M(String) stringWithFormat:@"\n"
+
+    "/*! @note This is an AUTOMATICALLY generated file!\n"
+    "    Built on %@ from %@ */\n", [NSDateFormatter localizedStringFromDate:NSDate.date
+                                                                   dateStyle:NSDateFormatterMediumStyle
+                                                                   timeStyle:NSDateFormatterMediumStyle], path];
+
+  for (id entry in [NSArray arrayWithContentsOfFile:path]) { [head appendString:@"\n"];
+
+    [entry isKindOfClass:NSString.class] ||
+    [entry  isKindOfClass:NSArray.class] ? [head appendString:
+    [entry isKindOfClass:NSString.class] ? entry : [entry componentsJoinedByString:@"\n"]] :
+
+    [entry enumerateKeysAndObjectsUsingBlock:^(NSString* kind, id defs, BOOL* s) {
+
+      [kind    hasPrefix:@"define"] ? [head _if:kind do:^{ for (id def in defs) [head _addDefine:def]; }]:
+
+      [kind isEqualToString:@"map"] ? [head _appendMap:defs] : nil;
+    }];
+  }
+  return head;
+}
+
+id _uSort(id str) { return _sTask($(@"echo \"%@\"|sort|uniq", str)); }
+
+id _sTask(id cmd) { return !cmd ? nil : ({ NSPipe* pipe; NSTask * task;
+
+  [task = NSTask.new setValuesForKeysWithDictionary: @{  @"launchPath" : @"/bin/zsh",
+                                                          @"arguments" : @[@"-c", cmd],
+                                                     @"standardOutput" : pipe = NSPipe.pipe}]; [task launch];
+  [NSString.alloc initWithData: pipe.fileHandleForReading.readDataToEndOfFile encoding:NSUTF8StringEncoding]; });
+}
+
+int main() { @autoreleasepool {
+
+  NSString* thisFile = [NSString stringWithUTF8String:__FILE__],
+           * srcroot = thisFile.stringByDeletingLastPathComponent,
+           * projNme = srcroot.lastPathComponent,
+             * plist = args.count > 1 ? args[1] : $(@"%@/%@.plist", srcroot, projNme);
+
+  id genHeader = _hMake(plist); NSError *e = nil;
+
+  NSLog(@"genheader %@", genHeader);
+  
+  for(id x in @[ $(@"%s/%@.h", BUILT_PRODUCTS_DIR, projNme),
+                 $(@"%@/%@.h", srcroot,            projNme)])
+
+    [genHeader writeToFile:x atomically:YES encoding:NSUTF8StringEncoding error:&e]; !e ?: NSLog(@"error:%@",e);
+
+  for (id arg in args) ![arg isMatch:RX(@".*\\.(m|h)$")] ?: NSLog(@"Need to preprocess %@", arg);
+
+  NSLog(@"pointers: %@, primitives: %@", pointers, primitives);
+
+} return EXIT_SUCCESS; }
+
+/*
+int _longest(id strings);
+
+
+int _longest(id strings) {
+    return (int)
+
+        [[[strings sortedArrayUsingDescriptors:
+                       @[ [NSSortDescriptor.alloc initWithKey:@"length" ascending:YES selector:@selector(localizedStandardCompare:)] ]] firstObject] length];
+}
+
 
 @interface      HeaderWriter : NSObject @end
 @implementation HeaderWriter { NSMutableString* _outString; id _plist; }
 
-- initWithPlist:(NSString*)list {
+- initWithPlist:path { return self = super.init &&
 
-  return self = super.init &&
-       [NSFileManager.defaultManager fileExistsAtPath:_plist = list isDirectory:NULL] ? self : nil;
+  [NSFileManager.defaultManager fileExistsAtPath:_plist = list isDirectory:NULL] ? self : nil;
 }
 - (NSMutableString*) outString { return _outString = _outString ?: [NSMutableString plist2Header:_plist]; }
 @end
-
-int main(int argc, char*argv[], char**argp) { @autoreleasepool {
-
-//  id c = _system(@"/xbin/isp");
-  _system([@"say word was: " stringByAppendingString:XcodePath]);
-
-
-  NSString * thisFile  = [NSString stringWithUTF8String:__FILE__],
-           * srcroot   = thisFile.stringByDeletingLastPathComponent,
-           * projName  = srcroot.lastPathComponent;
-
-  HeaderWriter *r = [HeaderWriter.alloc initWithPlist:argc > 1 ?
-                    [NSString stringWithUTF8String:argv[1]] :
-                    [NSString stringWithFormat:@"%@/%@.plist", srcroot, projName]];
-
-  NSMutableArray *outfiles = @[[NSString stringWithFormat:@"%s/%@.h",BUILT_PRODUCTS_DIR,projName],
-                               [NSString stringWithFormat:@"%@/%@.h",srcroot, projName]].mutableCopy;
-
-  NSLog(@"args: %@", NSProcessInfo.processInfo.arguments);
-
-  if (argc > 2) { int argcc = 2;
-
-    while (argcc < argc) { [outfiles addObject:[NSString stringWithUTF8String:argv[argcc]]]; argcc++; }
-  }
-
-  if (!r) return 99;
-
-  for (id outPath in outfiles) {
-    NSError *e = nil;
-    [r.outString writeToFile:outPath atomically:YES encoding:NSUTF8StringEncoding error:&e];
-    !e ?: NSLog(@"error:%@", e);
-  }
-
-}	return EXIT_SUCCESS; }
 
 //// The NSRegularExpression class is currently only available in the Foundation framework of iOS 4
 //NSError *error = NULL;
@@ -191,3 +204,5 @@ int main(int argc, char*argv[], char**argp) { @autoreleasepool {
 //  if (type)
 }
 @end
+*/
+
