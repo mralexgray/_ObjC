c290134e7cf271d8e6ee17f7438ada6654aed9bb
Alex Gray <alex@mrgray.com>
2015-04-18 10:05:03 -0400

    function decl + types to PCH, static functions etc

--- _ObjC/_ObjC_Tool.m
+++ _ObjC/_ObjC_Tool.m
@@ -1,66 +1,29 @@
 
-#define       M(X)       NSMutable##X
-#define       $(...)     [NSString stringWithFormat:__VA_ARGS__]
-#define      CR(MSTR)    [MSTR appendString:@"\n"]
-#define     LOC(S,LOC)   [S rangeOfString:LOC].location
-#define  APPEND(X,...)   [X appendFormat:__VA_ARGS__]
-#define      FM        NSFileManager.defaultManager
-#define    ARGS        NSProcessInfo.processInfo.arguments
-#define THEDATE        [NSDateFormatter localizedStringFromDate:NSDate.date \
-dateStyle:NSDateFormatterMediumStyle \
-timeStyle:NSDateFormatterMediumStyle]
-
-#import <Foundation/Foundation.h>
-
-NSString* AZEnumToBinary(int num) {  char str[9] = {0};
-
-  return $(@"%s", ({ int i; for(i=7;i>=0;i--){ str[i] = (num&1)?'1':'0'; num >>= 1; } str; }));
-}
-
-@interface RxMatch : NSObject
-
-@property (nonatomic)       NSRange   range;    // The range of the original string that was matched.
-@property (nonatomic,copy)  NSArray * groups;   // Group ONLY. Each object is an RxMatchGroup.
-@property (nonatomic,copy) NSString * value,    // The substring that matched the expression.
-                                    * original; // Group ONLY. The full original string that was matched against.
-@end
-@implementation  NSString (SubstringToOrFrom)
-
-- (NSString*) refactor { NSDictionary *rules = @{}; return @""; }
-
-- objectAtIndexedSubscript:(NSInteger)i { return i < 0 ? [self substringFromIndex:ABS(i)] : [self substringToIndex:i]; }
-
-@end
-
-NSString * Header(); NSDictionary * Plist(); NSString * Template (); static id templatePath, plistPath; NSDictionary * ParseArgs (); id replace(NSRegularExpression*,NSString*,NSString*(^)(RxMatch*)); id ObjectForAnyKeyPassingTest(NSDictionary*,NSArray*,BOOL(^)(id)); id _genTokens (id,id);
 
 int main() { @autoreleasepool {
 
-  BOOL(^ IsFileAndExists)(id) = ^BOOL(id p){ BOOL dir = NO;  return [FM fileExistsAtPath:p isDirectory:&dir] && !dir; };
+       plistPath = ObjectForAnyKeyPassingTest(ParseArgs(), @[@"d",@"p",@"data",@"model",@"plist"], IsFileAndExists);
+    templatePath = ObjectForAnyKeyPassingTest(ParseArgs(), @[@"t",@"template",@"header"],          IsFileAndExists);
+          output = ObjectForAnyKeyPassingTest(ParseArgs(), @[@"o",@"output"],                      NULL);
 
-  id      opts = ParseArgs();
-     plistPath = ObjectForAnyKeyPassingTest(opts, @[@"d",@"p",@"data",@"model",@"plist"], IsFileAndExists);
-  templatePath = ObjectForAnyKeyPassingTest(opts, @[@"t",@"template",@"header"],     IsFileAndExists);
-  id    output = ObjectForAnyKeyPassingTest(opts, @[@"o",@"output"],                 NULL);
+    if (!plistPath || !templatePath || !PlistDataModel() || !HeaderTemplate() || !CompiledHeader())
 
-  if (!plistPath || !templatePath || !Plist() || !Template() || !Header())
+      return fprintf(stdout, "%s\n ERROR. Need valid input for both plist data and header template!, got %s\nmodel:%s\ntemplate:%s", [ARGS[0] UTF8String], [ParseArgs() description].UTF8String, [plistPath UTF8String], [templatePath UTF8String]), EXIT_FAILURE;
 
-    return fprintf(stdout, "%s\n ERROR. Need valid input for both plist data and header template!, got %s\nmodel:%s\ntemplate:%s", [ARGS[0] UTF8String], [opts description].UTF8String, [plistPath UTF8String], [templatePath UTF8String]), EXIT_FAILURE;
+    if (!output) return fprintf(stdout, "%s", [CompiledHeader() UTF8String]);
 
-  if (!output) return fprintf(stdout, "%s", [Header() UTF8String]);
+    for (id place in [output isKindOfClass:NSString.class] ? @[output] : output) {
 
-  for (id place in [output isKindOfClass:NSString.class] ? @[output] : output)
+      fprintf(stdout, "Outputting header to destination:%s\n", [place UTF8String]);
 
-    if (![Header() writeToFile:place atomically:YES encoding:NSUTF8StringEncoding error:nil]) return EXIT_FAILURE;
+      if (![CompiledHeader() writeToFile:place atomically:YES encoding:NSUTF8StringEncoding error:nil]) return EXIT_FAILURE;
+    }
+  }
+  return EXIT_SUCCESS;
+}
 
-} return EXIT_SUCCESS; }
-
-NSDictionary * Plist    () { static id data;  return data  = data ?:  [NSDictionary dictionaryWithContentsOfFile:plistPath]; }
-    NSString * Template () { static id templ; return templ = templ ?: [NSString stringWithContentsOfFile:templatePath encoding:NSUTF8StringEncoding error:nil]; }
-
-typedef NS_OPTIONS(int, _Emit) { e_DEFS, e_TYPE      = 0x00000001,
-                                         e_TYPE_BLKS = 0x00000010 | e_TYPE,
-                                         e_TYPE_PMAP = 0x00000100 | e_TYPE};
+static NSDictionary * PlistDataModel () { static id data;  return data  = data ?:  [NSDictionary dictionaryWithContentsOfFile:plistPath]; }
+static     NSString * HeaderTemplate () { return template = template ?: [NSString stringWithContentsOfFile:templatePath encoding:NSUTF8StringEncoding error:nil]; }
 
 NSString * _genTokens(NSDictionary *plistData, NSString*head) {
 
@@ -119,7 +82,7 @@ NSString * _genTokens(NSDictionary *plistData, NSString*head) {
 
       !emit & e_TYPE ?: AS_ARG_SHORTCUTS(defs[k]); // if NOT a type, generate method argument variations
       
-      ![Plist()[@"ALIAS_INSTANCE_RETURN"] containsObject:defs[k]] ?: METHOD_RETURN_SHORTCUT(defs[k]);
+      ![PlistDataModel()[@"ALIAS_INSTANCE_RETURN"] containsObject:defs[k]] ?: METHOD_RETURN_SHORTCUT(defs[k]);
     };
 
     for (NSString *k in sortedKs) writeTypeOrDef(k);
@@ -154,24 +117,7 @@ RxMatch * result2Match(NSTextCheckingResult* result, NSString* original) {
   return match;
 }
 
-id replace(NSRegularExpression*self, NSString *string, NSString*(^replacer)(RxMatch*)) {
-
-  if (!replacer) return string;                   // no replacer? just return
-
-  NSMutableString* replaced = string.mutableCopy; //copy the string so we can replace subsections
-  NSArray* matches = [self matchesInString:string options:0 range:NSMakeRange(0, string.length)]; //get matches
-  for (int i=(int)matches.count-1; i>=0; i--) { //replace each match (right to left so indexing doesn't get messed up)
-
-    NSTextCheckingResult* result = matches[i];
-
-    [replaced replaceCharactersInRange:result.range
-                            withString:
-    /* replacement */ replacer(/*RxMatch* match */result2Match(result,string))];
-  }
-  return replaced;
-}
-
-id ObjectForAnyKeyPassingTest(NSDictionary *values, NSArray*okKeys, BOOL(^test)(id)) {
+id ObjectForAnyKeyPassingTest(NSDictionary *values, NSArray*okKeys, BOOL(*test)(id)) {
 
   __block id found = nil;
   [okKeys enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
@@ -181,44 +127,73 @@ id ObjectForAnyKeyPassingTest(NSDictionary *values, NSArray*okKeys, BOOL(^test)(
   return found;
 }
 
+static NSString * CompiledHeader() { return compiled = compiled ?: ({
 
-NSDictionary * ParseArgs() { id opts = @{}.mutableCopy, flag = nil;
+    NSRegularExpression *self = [NSRegularExpression.alloc initWithPattern:@"%% (.*) %%" options:0 error:nil];
 
-  for (NSString *argv in [ARGS subarrayWithRange:(NSRange){1,ARGS.count-1}]) { BOOL isFlag = [argv hasPrefix:@"-"];
+    NSString*(^replacer)(RxMatch*) = ^NSString*(RxMatch*match) {
 
-    id arg = !isFlag ? argv : ({ id newFlag = argv.copy; while ([newFlag hasPrefix:@"-"])
-      newFlag = [newFlag substringFromIndex:1]; newFlag; });
+      return _genTokens(PlistDataModel(),[match.groups.lastObject value]);
+    };
 
-    flag ? ({ isFlag && ![opts objectForKey:flag = arg] ? ({ opts[flag] = NSNull.null; }) : ({
+    NSString* string = //copy the string so we can replace subsections
 
-      id existing = opts[flag]; // doesn't have - prefix ... adding or creating a value.
+      $(@"\n/*!       @note This file is AUTO_GENERATED! Changes will NOT persist!\n"
+         "                Built on %@ from template:%@ with data from:%@ */\n%@",
+       THEDATE, [plistPath lastPathComponent], [templatePath lastPathComponent],HeaderTemplate());
 
-      opts[flag] = !existing || [existing isKindOfClass: NSNull.class] ? arg :
-      [existing isKindOfClass:NSArray.class] ?
-      [existing     arrayByAddingObject:arg] : @[existing, arg]; });
+    NSMutableString *replaced = string.mutableCopy;
 
-    }) : ({ isFlag && !opts[flag = arg] ? ({ opts[flag] = NSNull.null; })
+    NSArray* matches = [self matchesInString:string options:0 range:NSMakeRange(0, string.length)]; //get matches
 
-      : ({ opts[@"?"] = [opts[@"?"] ?: @[] arrayByAddingObject:arg]; flag = nil; }); // No '-', add to unnamed array.
-    });
+    for (int i= (int)matches.count-1; i>=0; i--) { //replace each match (right to left so indexing doesn't get messed up)
 
-  }  return [opts copy];
-} // Parses CLI to dictionary.  Generic, chic, and unrelated to task at hand.
+      NSTextCheckingResult* result = matches[i];
+
+      [replaced replaceCharactersInRange:result.range
+                              withString:
+      /* replacement */ replacer(/*RxMatch* match */result2Match(result,string))];
+    }
+    [replaced copy];
+  });
+}
+
+@implementation  NSString (SubstringToOrFrom)
+
+- (NSString*) refactor { NSDictionary *rules = @{}; return @""; }
+
+- objectAtIndexedSubscript:(NSInteger)i { return i < 0 ? [self substringFromIndex:ABS(i)] : [self substringToIndex:i]; }
+
+@end
 
 @implementation RxMatch @end
 
+static NSDictionary * ParseArgs() { static id opts; static dispatch_once_t onceToken; dispatch_once(&onceToken, ^{
 
-NSString * Header() { static id h; return h = h ?:
+   id _opts = @{}.mutableCopy, flag = nil;
 
-  replace([NSRegularExpression.alloc initWithPattern:@"%% (.*) %%" options:0 error:nil],
+    for (NSString *argv in [ARGS subarrayWithRange:(NSRange){1,ARGS.count-1}]) { BOOL isFlag = [argv hasPrefix:@"-"];
 
-   $(@"\n/*!       @note This file is AUTO_GENERATED! Changes will NOT persist!\n"
-     "                Built on %@ from template:%@ with data from:%@ */\n%@",
+      id arg = !isFlag ? argv : ({ id newFlag = argv.copy; while ([newFlag hasPrefix:@"-"])
+        newFlag = [newFlag substringFromIndex:1]; newFlag; });
 
-     THEDATE, [plistPath lastPathComponent], [templatePath lastPathComponent],Template()),
+      flag ? ({ isFlag && ![_opts objectForKey:flag = arg] ? ({ _opts[flag] = NSNull.null; }) : ({
 
-      ^NSString *(RxMatch *match) {
+        id existing = _opts[flag]; // doesn't have - prefix ... adding or creating a value.
 
-          return _genTokens(Plist(),[match.groups.lastObject value]); });
-}
+        _opts[flag] = !existing || [existing isKindOfClass: NSNull.class] ? arg :
+        [existing isKindOfClass:NSArray.class] ?
+        [existing     arrayByAddingObject:arg] : @[existing, arg]; });
+
+      }) : ({ isFlag && !_opts[flag = arg] ? ({ _opts[flag] = NSNull.null; })
+
+        : ({ _opts[@"?"] = [_opts[@"?"] ?: @[] arrayByAddingObject:arg]; flag = nil; }); // No '-', add to unnamed array.
+      });
+
+    }  opts = [_opts copy];
+  });
+
+  return opts;
+
+} // Parses CLI to dictionary.  Generic, chic, and unrelated to task at hand.
 
